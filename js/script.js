03ed61b7ecec46b6d3bbb8887bd15e2380fd55f8
Raywong <44193890+raywongstudy@users.noreply.github.com>
2022-01-13 18:27:36 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        data: this.data,
+        random_num: 0,
+        question: {},
+        show_alert: false,
+        question_result: true,
+        question_number: 0,
+        ans_key: "",
+        current_question_status: false,
+    },
+    created(){
+      this.random_num = this.getRandomInt(0, data.length)
+      this.question = this.data[this.random_num]
+    },
+    methods:{
+        getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+        },
+        SubmitAnswer(key){
+            if(this.ans_key ==this.question.answer){
+                this.question_result = true;
+            }else{
+                this.question_result = false;
+            }
+            this.show_alert = true;
+        },
+        GoNextQuestion(){
+            this.question_number += 1
+            this.show_alert = false
+            this.random_num = this.getRandomInt(0, data.length)
+            this.question = this.data[this.random_num]
+            document.querySelectorAll('input[name=ans]').forEach((element) => { element.checked = false })
+        },
+        GoNextQuestionW(){
+            this.show_alert = false
+            this.random_num = this.getRandomInt(0, data.length)
+            this.question = this.data[this.random_num]
+            document.querySelectorAll('input[name=ans]').forEach((element) => { element.checked = false })
+        }
+    }
+})
