6ced022475ec6969849990246565ec1c0368780b
Raywong <44193890+raywongstudy@users.noreply.github.com>
2022-01-14 18:10:32 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var app = new Vue({
         question_number: 0,
         ans_key: "",
         current_question_status: false,
+        had_select: false,
     },
     created(){
       this.random_num = this.getRandomInt(0, data.length)
@@ -21,19 +22,25 @@ var app = new Vue({
             return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
         },
         SubmitAnswer(key){
-            if(this.ans_key ==this.question.answer){
-                this.question_result = true;
+            if(this.had_select == true){
+                if(this.ans_key ==this.question.answer){
+                    this.question_result = true;
+                }else{
+                    this.question_result = false;
+                }
+                this.show_alert = true;
             }else{
-                this.question_result = false;
+                alert("請先選擇一個答案");
             }
-            this.show_alert = true;
         },
         GoNextQuestion(){
-            this.question_number += 1
-            this.show_alert = false
+            this.had_select = false
+            this.question_number += 1;
+            this.show_alert = false;
             this.random_num = this.getRandomInt(0, data.length)
             this.question = this.data[this.random_num]
             document.querySelectorAll('input[name=ans]').forEach((element) => { element.checked = false })
+            
         },
         GoNextQuestionW(){
             this.show_alert = false
